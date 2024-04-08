fc533d8059b2b8427dbc7fc11cac85948e5e207d
Sylvester009 <sasy260902@gmail.com>
2024-04-08 21:44:33 +0100

    finished project

--- src/components/HomeContainer.jsx
+++ src/components/HomeContainer.jsx
@@ -5,20 +5,17 @@ import { heroData } from "../utils/data";
 
 const HomeContainer = () => {
   return (
-    <section
-      className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full "
-      id="home"
-    >
+    <section className="grid grid-cols-1 md:grid-cols-2 gap-2 w-full" id="home">
       <div className="py-2 flex-1 flex flex-col items-start justify-center gap-6">
-        <div className="flex items-center gap-2 justify-center bg-orange-100 px-4 py-1 rounded-full">
+        <div className="flex items-center gap-2 justify-center bg-orange-200 px-4 py-1 rounded-full">
           <p className="text-base text-orange-500 font-semibold">
             Bike Delivery
           </p>
           <div className="w-8 h-8 bg-white rounded-full overflow-hidden drop-shadow-xl">
             <img
               src={Delivery}
-              className="w-full h-full object-contain"
               alt="delivery"
+              className="w-full h-full object-contain"
             />
           </div>
         </div>
@@ -44,19 +41,20 @@ const HomeContainer = () => {
           Order Now
         </button>
       </div>
+
       <div className="py-2 flex-1 flex items-center relative">
         <img
           src={HeroBg}
-          className=" ml-auto h-420 w-full lg:w-auto lg:h-650"
+          className="ml-auto h-420 w-full lg:w-auto lg:h-650"
           alt="hero-bg"
         />
 
-        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-32  py-4 gap-4 flex-wrap">
-          {heroData &&
+        <div className="w-full h-full absolute top-0 left-0 flex items-center justify-center lg:px-16 py-4 gap-4 flex-wrap">
+        {heroData &&
             heroData.map((n) => (
               <div
                 key={n.id}
-                className="  lg:w-190  p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
+                className="lg:w-190 p-4 bg-cardOverlay backdrop-blur-md rounded-3xl flex flex-col items-center justify-center drop-shadow-lg"
               >
                 <img
                   src={n.imageSrc}
